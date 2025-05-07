8bc86c87f1f15f3adf59794fcb24434d8e3dd69b
Syed Ayatullah Kareem <kareem.the.programmer@gmail.com>
2025-05-07 15:57:08 +0530

    update

--- config.js
+++ config.js
@@ -5,18 +5,18 @@ introDesc: "I don’t know how you came into my life, maybe it was a coincidence
 
 btnIntro: 'Tap to see',
 
-title: 'I love you Isha💞, and this is my way of saying it',
+title: 'I love you Isha, and this is my way of saying it',
 desc: 'My feelings are true, and they grow every day.',
 
-btnYes: 'I love you too',
-btnNo: 'No, I don’t feel that way',
+btnYes: 'I love you too, Kareem',
+btnNo: 'No, I don’t love you',
 
-question: 'So… out of the whole world, why do I love you?',
-btnReply: 'Tell me',
-reply: 'Because you’re you. And that’s more than enough for me.',
+question: 'to..., poori duniya chhod ke me aapse hi kyu pyaar krta hun?',
+btnReply: 'Socho🕊️',
+reply: 'Kyuki aap aap ho, aur sirf yahi mere liye kaafi hai.',
 
-mess: 'I knew you’d understand. My heart is yours.',
-messDesc: `Now your question: "What can I do for you?"
+mess: 'Mujhe Pata tha aap samjhogi😌 mera ye dil aapka h💝',
+messDesc: `Now your question: "Me apke liye kya krskta hun?"
 Everything which I can. jab aap akela feel krogi to aapke sath rahunga, jab aap udaas hogi to aapko hasaunga, jab aap ro rhi hogi to aapko manaunga, aur jab aap khush hogi to aapke dimagh ka khichdi banaunga. I'll be always there for you💖`,
 
 btnAccept: 'I’m yours <3',
